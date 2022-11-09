f0361411d634be0d2355d06ec1bec2ea3536917e
Robert Schauner <rds156@case.edu>
2022-11-09 15:51:58 -0500

    update code

--- GDC.R
+++ GDC.R
@@ -8,60 +8,9 @@ library(ggplot2)
 library(here)
 library(broom)
 library(ggsignif)
+source("lib/import.R")
 
 plan(multicore)
-
-ReadNIHData <- function(path) {
-    # Read in data from GDC Commons
-    # Args:
-    #   path: path to data (UUID subdirectories are allowed)
-    # Returns:
-    #   list of data.tables
-    files <- list.files(path = path, pattern = ".*.tsv$", full.names = TRUE, recursive = TRUE)
-    data <- future_lapply(files, fread, select = c("gene_name", "unstranded"))
-    x <- tools::file_path_sans_ext(basename(files))
-    names(data) <- str_remove(x, ".rna_seq.augmented_star_gene_counts")
-    return(data)
-}
-
-ReadStJudeData <- function(path) {
-    # Read in data from St. Jude Cloud
-    # Args:
-    #   path: path to data (UUID subdirectories are not allowed)
-    # Returns:
-    #   list of data.tables
-    files <- list.files(path = path, pattern = ".*.txt$", full.names = TRUE)
-    data <- future_lapply(files, fread, header = FALSE, col.names = c("gene_name", "unstranded"))
-    x <- tools::file_path_sans_ext(basename(files))
-    names(data) <- str_remove(str_extract(x, "^.+_"), "_")
-    return(data)
-}
-
-ReadStJudeAnnotation <- function(path) {
-    # Read in annotation from St. Jude Cloud
-    # Args:
-    #   path: path to annotation file
-    # Returns:
-    #   data.table
-    cols <- c("subject_name", "sample_name", "sj_long_disease_name", "attr_sex", "attr_age_at_diagnosis")
-    ann <- fread(path, select = cols)
-    new <- c("patient", "sample_id", "disease", "sex", "age")
-    setnames(ann, cols, new)
-    ann[, .id := sample_id]
-}
-
-ReadNIHAnnotation <- function(path) {
-    # Read in annotation from GDC Commons
-    # Args:
-    #   path: path to annotation file
-    # Returns:
-    #   data.table
-    cols <- c("case_id", "case_submitter_id", "primary_diagnosis", "gender", "age_at_diagnosis")
-    ann <- fread(path, select = cols, header = TRUE)
-    new <- c("patient", "sample_id", "disease", "sex", "age")
-    setnames(ann, cols, new)
-    ann[, .id := patient][ , age := as.numeric(age) / 365]
-}
 
 SimplifyDiseaseNames <- function(s) {
     # Simplify disease names
